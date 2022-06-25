b73a8193e91b09d5d21c12753c6eef9307724eea
yujiale <yujiale@authing.cn>
2022-06-25 21:49:54 +0800

    fix：原型模式之浅拷贝基本原理

--- src/main/java/com/yjl/design/creation/prototype/shallowCopy/MainTest.java
+++ src/main/java/com/yjl/design/creation/prototype/shallowCopy/MainTest.java
@@ -7,9 +7,7 @@ import java.util.List;
 /**
  * @author yujiale
  *
- * 原型模式的浅拷贝只会复制对象中基本数据类型和引用对象的内存地址，不会递归地传递复制引用对象以及引用对象的引用对象
- * 克隆后的对象与原对象的数据都是共享的
- * Object 类的 clone 方法只会拷贝对象中的基本数据类型，比如（int、Long、float）等以及引用对象的内存地址，不会拷贝引用对象本身。
+
  */
 public class MainTest {
 
