a395c1282317206b65f88932ee57294377815a37
yujiale <yujiale@authing.cn>
2022-06-25 21:19:24 +0800

    feat：原型模式之浅拷贝基本原理

--- src/main/java/com/yjl/design/creation/prototype/shallowCopy/MainTest.java
+++ src/main/java/com/yjl/design/creation/prototype/shallowCopy/MainTest.java
@@ -3,10 +3,13 @@ package com.yjl.design.creation.prototype.shallowCopy;
 import java.util.ArrayList;
 import java.util.List;
 
+
 /**
- * @program: DesignPatterns
- * @author: yjl
- * @created: 2022/01/20
+ * @author yujiale
+ *
+ * 原型模式的浅拷贝只会复制对象中基本数据类型和引用对象的内存地址，不会递归地传递复制引用对象以及引用对象的引用对象
+ * 克隆后的对象与原对象的数据都是共享的
+ * Object 类的 clone 方法只会拷贝对象中的基本数据类型，比如（int、Long、float）等以及引用对象的内存地址，不会拷贝引用对象本身。
  */
 public class MainTest {
 
@@ -21,12 +24,11 @@ public class MainTest {
 
         ConcretePrototype cloneType = concretePrototype.clone();
         cloneType.getHobbies().add("技术");
-
         cloneType.setName("李四");
-        cloneType.setAge(30);
         System.out.println("原型对象：" + concretePrototype);
         System.out.println("克隆对象：" + cloneType);
         System.out.println(concretePrototype == cloneType);
+        System.out.println(concretePrototype.getAge().equals(cloneType.getAge()));
         System.out.println("原型对象的爱好：" + concretePrototype.getHobbies());
         System.out.println("克隆对象的爱好：" + cloneType.getHobbies());
         System.out.println(concretePrototype.getHobbies() == cloneType.getHobbies());
