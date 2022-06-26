4e82eff5a784fb90eca64a4195955512c6f614fb
yujiale <yujiale@authing.cn>
2022-06-26 15:31:58 +0800

    fix：原型模式实战之mybatis和sql

---
+++ src/main/java/com/yjl/design/creation/prototype/actualcombat/sql/RealizeDeepCopyMethod03.java
@@ -0,0 +1,39 @@
+package com.yjl.design.creation.prototype.actualcombat.sql;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @Author yujiale
+ * @Date 2022/6/26 15:29
+ * @Description TODO
+ **/
+public class RealizeDeepCopyMethod03 {
+    private HashMap<String, SearchWord> currentKeywords = new HashMap<>();
+    private long lastUpdateTime = -1;
+
+    public void refresh() {
+//Shallowcopy
+        HashMap<String, SearchWord> newKeywords = (HashMap<String, SearchWord>) currentKeywords;
+
+//从数据库中取出更新时间 >lastUpdateTime 的数据，放入到newKeywords 中
+        List<SearchWord> toBeUpdatedSearchWords = getSearchWords(lastUpdateTime);
+        long maxNewUpdatedTime = lastUpdateTime;
+        for (SearchWord searchWord : toBeUpdatedSearchWords) {
+            if (searchWord.getLastUpdateTime() > maxNewUpdatedTime) {
+                maxNewUpdatedTime = searchWord.getLastUpdateTime();
+            }
+            if (newKeywords.containsKey(searchWord.getKeyword())) {
+                newKeywords.remove(searchWord.getKeyword());
+            }
+            newKeywords.put(searchWord.getKeyword(), searchWord);
+        }
+        lastUpdateTime = maxNewUpdatedTime;
+        currentKeywords = newKeywords;
+    }
+
+    private List<SearchWord> getSearchWords(long lastUpdateTime) {
+//TODO:从数据库中取出更新时间 >lastUpdateTime 的数据
+        return null;
+    }
+}
