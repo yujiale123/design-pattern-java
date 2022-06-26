86d9a720eb0982dcf2c38ae5f729b1fa8ac282fe
yujiale <yujiale@authing.cn>
2022-06-26 16:54:30 +0800

    feat：单例模式之内部静态类反射攻击

--- src/main/java/com/yjl/design/creation/singleton/InnerClassSingleton/MainTest.java
+++ src/main/java/com/yjl/design/creation/singleton/InnerClassSingleton/MainTest.java
@@ -1,12 +1,19 @@
 package com.yjl.design.creation.singleton.InnerClassSingleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * @Author yujiale
  * @Date 2022/6/26 16:45
  * @Description TODO
  **/
 public class MainTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        test02();
+    }
+
+    public  static  void test01(){
         InnerClassSingleton instance = InnerClassSingleton.getInstance();
         InnerClassSingleton instance1 = InnerClassSingleton.getInstance();
         System.out.println(instance == instance1);
@@ -20,5 +27,14 @@ public class MainTest {
             InnerClassSingleton instance2 = InnerClassSingleton.getInstance();
             System.out.println(instance2);
         }).start();
+    }
+
+    public  static  void test02() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Constructor<InnerClassSingleton> declaredConstructor = InnerClassSingleton.class.getDeclaredConstructor();
+        declaredConstructor.setAccessible(true);
+        InnerClassSingleton innerClassSingleton = declaredConstructor.newInstance();
+        InnerClassSingleton instance = InnerClassSingleton.getInstance();
+        System.out.println(instance == innerClassSingleton);
+
     }
 }
