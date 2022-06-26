cbc2761a1aea9ca155fabae825aa6333abca3bcc
yujiale <yujiale@authing.cn>
2022-06-26 16:55:52 +0800

    feat：单例模式之内部静态类反射攻击

--- src/main/java/com/yjl/design/creation/singleton/InnerClassSingleton/InnerClassSingleton.java
+++ src/main/java/com/yjl/design/creation/singleton/InnerClassSingleton/InnerClassSingleton.java
@@ -8,6 +8,8 @@ package com.yjl.design.creation.singleton.InnerClassSingleton;
  * 静态内部类实现单例模式
  *  本质上是利用类的加载机制来保证线程安全
  *  只有在实际使用的时候，才会触发类的初始化，本质上也是一种懒加载的一种形式
+ *
+ *  饿汉模式和静态内部类可以通过判断单例是否存在来防止反射攻击
  **/
 public class InnerClassSingleton {
     private static class InnerClassSingletonHolder {
@@ -16,6 +18,10 @@ public class InnerClassSingleton {
 
 
     private InnerClassSingleton() {
+
+        if (InnerClassSingletonHolder.instance == null) {
+            throw new RuntimeException("单例不允许多个示例");
+        }
 
     }
 
