a858114ac56b0d6994d62b32523b9a4569b56b75
yujiale <yujiale@authing.cn>
2022-06-26 19:24:58 +0800

    feat：工厂模式之工厂方法

---
+++ src/main/java/com/yjl/design/creation/factory/method/actual/test.java
@@ -0,0 +1,11 @@
+package com.yjl.design.creation.factory.method.actual;
+
+/**
+ * @Author yujiale
+ * @Date 2022/6/26 19:19
+ * @Description TODO
+ *
+ * 工厂方法是将对象的生成放到子类来完成，会存在多个对象和多个工厂，每个对象对应着工厂，如果添加新抽象对象，那么之前每个对象都要改动代码，修改工厂方法 违背OCP
+ **/
+public class test {
+}
