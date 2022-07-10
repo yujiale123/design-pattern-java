8331419597bd69696f21793e20285d01438215d5
yujiale <yujiale@authing.cn>
2022-07-10 21:29:05 +0800

    fix：update main.2312312

---
+++ src/main/java/com/yjl/design/basic/GetConstructor.java
@@ -0,0 +1,48 @@
+package com.yjl.design.basic;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * @program: DesignPatterns
+ * @author: yjl
+ * @created: 2022/02/07
+ */
+public class GetConstructor {
+
+    public static void main(String[] args) {
+        try {
+            Class<?> aClass = Class.forName("com.yjl.design.basic.Person");
+
+            //1、获取所有的public权限的构造器(getConstructors)
+            Constructor<?>[] constructors = aClass.getConstructors();
+
+            //注意查看构造器的顺序
+            for (Constructor<?> constructor : constructors) {
+                System.out.println("获取所有的public权限的构造器getConstructors:" + constructor);
+            }
+            System.out.println("-===============================");
+
+            //2、获取所有的构造器(getDeclaredConstructors)
+            Constructor<?>[] declaredConstructors = aClass.getDeclaredConstructors();
+            for (Constructor<?> declaredConstructor : declaredConstructors) {
+                System.out.println("获取所有的构造器getDeclaredConstructors:" + declaredConstructor);
+            }
+            System.out.println("-===============================");
+
+
+            //3、获取指定的构造器(放入具体的类型)
+            Constructor<?> constructor = aClass.getConstructor(Integer.class);
+            System.out.println("获取指定的构造器(放入具体的类型):" + constructor);
+            System.out.println("-===============================");
+            //非public权限
+            Constructor<?> declaredConstructor = aClass.getDeclaredConstructor(String.class, Integer.class);
+            System.out.println("获取指定的构造器(放入具体的类型):" + declaredConstructor);
+            System.out.println("-===============================");
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/yjl/design/basic/GetModifiersMethod.java
@@ -0,0 +1,25 @@
+package com.yjl.design.basic;
+
+import java.lang.reflect.Modifier;
+
+/**
+ * @program: DesignPatterns
+ * @author: yjl
+ * @created: 2022/02/07
+ */
+public class GetModifiersMethod {
+
+    public static void main(String[] args) {
+        try {
+            Class<?> aClass = Class.forName("com.yjl.design.basic.Person");
+
+            //获得修饰符
+            int modifiers = aClass.getModifiers();
+            //使用Modifier转换为相应的字符串
+            System.out.println("通过反射获取类的修饰符"+Modifier.toString(modifiers));
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
